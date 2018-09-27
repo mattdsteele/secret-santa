91384af54e56e1f81c8129598784627ffc99b182
mattdsteele <orphum@gmail.com>
2018-09-26 23:01:50 -0500

    Display and make a wishlist

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueMaterial from "vue-material";
+import VueMarkdown from 'vue-markdown';
 import App from "./App.vue";
 import router from "./router";
 import "vue-material/dist/vue-material.min.css";
@@ -12,6 +13,7 @@ import store from "./store";
 Vue.config.productionTip = false;
 
 Vue.use(VueMaterial);
+Vue.component('vue-markdown', VueMarkdown);
 
 new Vue({
   router,
--- src/views/List.vue
+++ src/views/List.vue
@@ -2,10 +2,11 @@
 <div>
 <h1>My List</h1>
 <md-field>
-    <label>Textarea with Autogrow</label>
+    <label>Your Wish List:</label>
     <md-textarea v-model="wishlist" md-autogrow></md-textarea>
 </md-field>
-{{ wishlist }}
+<hr>
+<vue-markdown :source="wishlist"></vue-markdown>
 </div>
     
 </template>
@@ -14,8 +15,28 @@
 import Vue from 'vue'
 import { Prop, Component } from 'vue-property-decorator';
 
+const defaultWishlist = `
+# My Wish List
+
+## Favorite Books
+
+* How to Win At Everything
+* Best Buds For Life
+
+## Favorite Stores
+
+* Foo
+* Bar
+`;
+
 @Component
 export default class List extends Vue {
-    public wishlist = '';
+    public wishlist = defaultWishlist;
 }
 </script>
+
+<style>
+textarea.md-textarea.md-textarea {
+    font-family: 'Consolas', Courier, monospace;
+}
+</style>
