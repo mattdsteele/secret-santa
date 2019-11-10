d4a626005d58623fe5f0bf2e4dea3c00688b5ca1
mattdsteele <orphum@gmail.com>
2019-11-10 10:33:54 -0600

    Update admin pages

--- admin/src/components/DefaultListMaker.js
+++ admin/src/components/DefaultListMaker.js
@@ -6,7 +6,9 @@ const defaultLists = functions.httpsCallable('makeDefaultLists');
 
 const mapStateToProps = state => {
   return {
-    users: state.users.usersList
+    users: state.users.usersList,
+    year: state.lists.year,
+    lists: state.lists.all
   };
 };
 const mapDispatchToProps = dispatch => {
@@ -22,16 +24,29 @@ class defaultListMaker extends Component {
   async makeDefaultLists() {
     const year = new Date().getFullYear();
     const res = await defaultLists({ year });
-    console.log(res);
     console.log(res.data);
   }
   render() {
+    if (!this.props.lists) {
+      return <h2>Default lists for {this.props.year}</h2>;
+    }
+    const currentYearLists = this.props.lists.filter(
+      l => l.year === this.props.year
+    );
     return (
       <div>
-        <h2>Default lists</h2>
-        <button onClick={() => this.makeDefaultLists()}>
-          Make default lists
-        </button>
+        <h2>Default lists for {this.props.year}</h2>
+        {currentYearLists.length === 0 ? (
+          <button onClick={() => this.makeDefaultLists()}>
+            Make default lists
+          </button>
+        ) : (
+          <p>
+            {' '}
+            {currentYearLists.length} lists already generated for{' '}
+            {this.props.year}
+          </p>
+        )}
       </div>
     );
   }
--- admin/src/store/index.js
+++ admin/src/store/index.js
@@ -1,9 +1,14 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers';
 const logger = createLogger();
 
-const store = createStore(rootReducer, {}, applyMiddleware(thunk, logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  {},
+  composeEnhancers(compose(applyMiddleware(thunk, logger)))
+);
 
 export { store };
