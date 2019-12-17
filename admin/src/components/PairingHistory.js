e27f1b6417a036263637cf22553933072713aba8
mattdsteele <orphum@gmail.com>
2019-12-16 20:24:33 -0600

    Make visibility conditionally shown

--- admin/src/components/PairingHistory.js
+++ admin/src/components/PairingHistory.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { getAllPairings } from '../store/actions';
 
@@ -14,30 +14,35 @@ const mapDispatchToProps = dispatch => {
 };
 
 const pairingHistory = props => {
+  const [visible, setVisible] = useState(false);
   const userFor = id => {
     return props.users.filter(u => u.uid === id)[0];
   };
   const name = id => userFor(id).displayName;
   return (
-    <div className="hidden">
-      <h1>Pairings History</h1>
-      {props.pairings && props.users && (
-        <table>
-          <thead>
-            <th>Year</th>
-            <th>Gifter</th>
-            <th>Giftee</th>
-          </thead>
-          {props.pairings.map(({ year, gifter, giftee }) => (
-            <tr>
-              <td>{year}</td>
-              <td>{name(gifter)}</td>
-              <td>{name(giftee)}</td>
-            </tr>
-          ))}
-        </table>
-      )}
-    </div>
+    <>
+      <h1 onClick={() => setVisible(!visible)}>
+        Pairings History (click to show)
+      </h1>
+      <div className={visible ? '' : 'hidden'}>
+        {props.pairings && props.users && (
+          <table>
+            <thead>
+              <th>Year</th>
+              <th>Gifter</th>
+              <th>Giftee</th>
+            </thead>
+            {props.pairings.map(({ year, gifter, giftee }) => (
+              <tr>
+                <td>{year}</td>
+                <td>{name(gifter)}</td>
+                <td>{name(giftee)}</td>
+              </tr>
+            ))}
+          </table>
+        )}
+      </div>
+    </>
   );
 };
 export const PairingHistory = connect(
