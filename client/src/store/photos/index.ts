158924a33a2adea761ffc34c4673c29d54bae53b
mattdsteele <orphum@gmail.com>
2020-10-24 12:10:18 -0500

    Update photos

--- client/src/store/photos/index.ts
+++ client/src/store/photos/index.ts
@@ -1,37 +1,36 @@
-import { RootState } from "@/store/types";
-import { ActionTree, Module, MutationTree } from "vuex";
-import { storage } from "../firestore";
+import { RootState } from '@/store/types';
+import { ActionTree, Module, MutationTree } from 'vuex';
+import { storage } from '../firestore';
 
 export interface PhotosState {
   photoUrl?: string;
 }
 
 const photoRefs = [
-  "img/IMG_20171225_105849.jpg",
-  "img/IMG_20171225_112618.jpg",
-  "img/IMG_20171225_112649.jpg",
-  "img/IMG_3901.jpg",
-  "img/IMG_3904.jpg",
-  "img/IMG_3905.jpg"
+  'img/IMG_20171225_105849.jpg',
+  'img/IMG_20171225_112618.jpg',
+  'img/IMG_20171225_112649.jpg',
+  'img/IMG_3904.jpg',
+  'img/IMG_3905.jpg',
 ];
 
 const actions: ActionTree<PhotosState, RootState> = {
   async init(ctxt) {
     const photo = photoRefs[Math.floor(Math.random() * photoRefs.length)];
     const url = await storage.ref(photo).getDownloadURL();
-    ctxt.commit("ref", url);
-  }
+    ctxt.commit('ref', url);
+  },
 };
 const mutations: MutationTree<PhotosState> = {
   ref(store, url: string) {
     store.photoUrl = url;
-  }
+  },
 };
 export const photos: Module<PhotosState, RootState> = {
   namespaced: true,
   state: {
-    photoUrl: undefined
+    photoUrl: undefined,
   },
   actions,
-  mutations
+  mutations,
 };
